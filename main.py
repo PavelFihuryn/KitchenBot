ee429fa660012b66a26ad986a33b886df6624f12
PavelFihuryn <pavelfihuryn@gmail.com>
2021-09-26 22:57:48 +0300

    Heroku tring first

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram import Bot, Dispatcher, executor
 from config import BOT_TOKEN
 
+import os
+
+PORT = int(os.environ.get('PORT', 5000))
 
 loop = asyncio.get_event_loop()
 bot = Bot(BOT_TOKEN, parse_mode="HTML")
@@ -14,5 +17,8 @@ if __name__ == '__main__':
     from handlers import dp, send_to_admin
 
     loop = asyncio.get_event_loop()
-    # loop.create_task(see())
     executor.start_polling(dp, on_startup=send_to_admin)
+    executor.start_webhook(listen="0.0.0.0",
+                           port=int(PORT),
+                           url_path=BOT_TOKEN)
+    executor.bot.setWebhook('https://kufarcitchenbot.herokuapp.com/' + BOT_TOKEN)
