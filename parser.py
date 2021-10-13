4f631d1af78d79e2a15602ad005a3e79f8e1e113
PavelFihuryn <pavelfihuryn@gmail.com>
2021-10-13 13:51:27 +0300

    find chair

--- parser.py
+++ parser.py
@@ -3,19 +3,15 @@ from bs4 import BeautifulSoup
 
 
 def get_links():
-    url = "https://www.kufar.by/l/r~minsk/kuhni?sort=lst.d&cnd=1&cur=BYR&query=%D0%BA%D1%83%D1%85%D0%BD%D1%8F&size=42"
+    url = "https://www.kufar.by/l/r~minsk/stulya-ofisnye-kompyuternye"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
-
-    links = soup.find('article', class_='kf-iwJ-a69a6')
-    if links:
-        links_list = links.find_all('a', class_='kf-wwee-3f4fa')
-    else:
-        return []
+    links = soup.find_all('article')
     kitchen = []
-    for link in links_list:
+    for link in links:
+        print(str(link.find('span'))[6:-7].split(',')[0])  # )
         if str(link.find('span'))[6:-7].split(',')[0] == 'Сегодня':
-            kitchen.append(link.get('href'))
+            kitchen.append(link.find('a').get('href'))
     return kitchen
 
 
